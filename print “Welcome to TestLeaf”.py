fb53bc28fddeedbececa7ed3fd776360aae8c399
padmavathykaruvadi-ai-related <padmavathykaruvadi@gmail.com>
2025-09-02 17:35:43 +0530

    Add files via upload

---
+++ print “Welcome to TestLeaf”.py
@@ -0,0 +1,12 @@
+print("Welcome to TestLeaf")
+
+age=int(input("Enter your age: "))
+experience=int(input("Enter your experience: "))
+print("My age is" ,age)
+print("My experience is" ,experience)
+if (age >= 22 and experience >= 2):
+    print("Access Granted")
+
+else:
+
+    print("Access Denied")
